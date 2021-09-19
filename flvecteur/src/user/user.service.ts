bd46930e5c6d43994b5e43488eaa1940dbfdc733
Fredlandeau <fredericlandeau@free.fr>
2021-09-19 17:39:36 +0200

    prisma

--- flvecteur/src/user/user.service.ts
+++ flvecteur/src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaClient, user } from '@prisma/client';
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'stdout',
+      level: 'query',
+    },
+    'info',
+    `warn`,
+    `error`,
+  ],
+});
+
 @Injectable()
-export class UserService {}
+export class UserService {
+  getAlluser(): Promise<user[]> {
+    return prisma.user.findMany({
+      include: {
+        commentaires: true,
+      },
+    });
+  }
+}
